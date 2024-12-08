b68bb2ab05ad43a716ed217802978e8d5ece7390
shashank189612 <shashanknm18@gmail.com>
2024-12-08 16:28:07 +0530

    created helloworld bean

--- src/main/java/com/iris/restWebServices/rest_web_services/HelloWorldController.java
+++ src/main/java/com/iris/restWebServices/rest_web_services/HelloWorldController.java
@@ -2,13 +2,23 @@ package com.iris.restWebServices.rest_web_services;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
-    @GetMapping("/helloWorld")
+    @GetMapping("/hello-world")
     public String helloWorld() {
         return "Hello World";
+    }
+
+
+    @GetMapping("/hello-world/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name) {
+
+        return new HelloWorldBean(  String.format("Hello %s!", name));
+
+
     }
 }
